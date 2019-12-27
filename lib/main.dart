6c9308b2b0cd35a1895d29a7e1c1383ed8ead249
ranjit1997 <44993852+ranjit1997@users.noreply.github.com>
2019-12-27 15:42:34 +0530

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+void main() {
+  runApp(new MaterialApp(
+    home: new HomePage(),
+  ));
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  HomePageState createState() => new HomePageState();
+}
+
+class HomePageState extends State<HomePage> {
+
+  List data;
+
+  Future<String> getData() async {
+    var response = await http.get(
+      Uri.encodeFull("https://creator.zoho.in/appbuilder/rk1220245/appliction-3/customFunction/function1/edit"),
+      headers: {
+        "Accept": "application/json"
+        
+      }
+    );
+    print(response.body);
+    // data = json.decode(response.body);
+    // print(data[1]["title"]);
+    
+    // return "Success!";
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: new Center(
+        child: new RaisedButton(
+          child: new Text("Get data"),
+          onPressed: getData,
+        ),
+      ),
+    );
+  }
+}
